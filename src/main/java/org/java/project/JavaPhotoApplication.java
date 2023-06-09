dc14107b1b2bf1f150415e40daffd68b7a97ad10
Luigi213 <reyesgianluigi88@gmail.com>
2023-06-09 09:35:33 +0200

    first commit

---
+++ src/main/java/org/java/project/JavaPhotoApplication.java
@@ -0,0 +1,13 @@
+package org.java.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPhotoApplication.class, args);
+	}
+
+}
